750c7a593284f3758e61e6852cc92ce27966d8b3
yangkwak <yangkwak@gmail.com>
2016-07-04 11:48:28 -0400

    update

---
+++ prototype/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prototype.Startup))]
+namespace prototype
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
